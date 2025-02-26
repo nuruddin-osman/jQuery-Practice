8457fa4c40da81e74f816676e874a86a6efa522e
mnosumon <mdnuruddin378350@gmail.com>
2025-02-26 16:08:02 +0600

    CSS Style manipulating

--- script.js
+++ script.js
@@ -9,17 +9,30 @@
 // $("#para").prepend("hi");
 
 // attribute manipulation
-document
-  .querySelector("a")
-  .setAttribute(
-    "href",
-    "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
-  );
-const link = document.querySelector("a").getAttribute("href");
-console.log(link);
-const ss = $("a").attr("href");
-console.log(ss);
-$("a").attr(
-  "href",
-  "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
-);
+// document
+//   .querySelector("a")
+//   .setAttribute(
+//     "href",
+//     "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
+//   );
+// const link = document.querySelector("a").getAttribute("href");
+// console.log(link);
+// const ss = $("a").attr("href");
+// console.log(ss);
+// $("a").attr(
+//   "href",
+//   "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
+// );
+
+// CSS Style manipulating
+
+$("p").css("color", "red");
+$("p").css("font-weight", "bold");
+$("p").css("font-size", "4rem");
+
+$("p").css({ "font-size": "4rem", "font-weight": "bold", color: "green" });
+
+document.querySelector("p").classList.add("para");
+document.querySelector("p").classList.remove("para1");
+$("p").addClass("para");
+$("p").removeClass("para1");
