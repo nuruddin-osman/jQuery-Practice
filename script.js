2686ae4ac63b8b357010f81de026f5f4bc53d05d
mnosumon <mdnuruddin378350@gmail.com>
2025-02-26 15:30:47 +0600

    attribute manipulation

--- script.js
+++ script.js
@@ -2,8 +2,24 @@
 // $("h2").text("Hello MOstak"); //jQuery foramte
 
 // text manipulation
-$("#headin").html("<p>Naeem</p>");
-const text01 = $("<p></p>").html("<b>hello Vai Miuje</b>");
-$("#para").append(text01);
+// $("#headin").html("<p>Naeem</p>");
+// const text01 = $("<p></p>").html("<b>hello Vai Miuje</b>");
+// $("#para").append(text01);
 
-$("#para").prepend("hi");
+// $("#para").prepend("hi");
+
+// attribute manipulation
+document
+  .querySelector("a")
+  .setAttribute(
+    "href",
+    "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
+  );
+const link = document.querySelector("a").getAttribute("href");
+console.log(link);
+const ss = $("a").attr("href");
+console.log(ss);
+$("a").attr(
+  "href",
+  "https://chatgpt.com/c/67be97a5-8324-8002-8179-f041f6bb1934"
+);
