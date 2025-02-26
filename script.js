97e7622c2c099b3846ffe63729da65dbabe750fa
mnosumon <mdnuruddin378350@gmail.com>
2025-02-26 14:30:45 +0600

    text manipulation

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.querySelector("h1").innerHTML = "Hi Sumon"; //javascript foramte
+// document.querySelector("h1").innerHTML = "Hi Sumon"; //javascript foramte
+// $("h2").text("Hello MOstak"); //jQuery foramte
 
-$("h2").text("Hello MOstak"); //jQuery foramte
+// text manipulation
+$("#headin").html("<p>Naeem</p>");
+const text01 = $("<p></p>").html("<b>hello Vai Miuje</b>");
+$("#para").append(text01);
+
+$("#para").prepend("hi");
